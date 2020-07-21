1d6a1ee04d2d39f8af084962fc3faf8efb79521d
obanb <ondrej.basista@apitree.cz>
2020-07-21 22:20:30 +0200

    m

--- src/components/Dashboard/components/Dashboard.tsx
+++ src/components/Dashboard/components/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { useApolloClient, useQuery } from '@apollo/react-hooks';
+import TableRow from './TableRow'
 
 import gql from 'graphql-tag';
 
@@ -22,10 +23,33 @@ export const query = {
 interface Props {}
 
 const Dashboard = ({}: Props) => {
+  const [data, setData] = React.useState([])
+  React.useEffect(() => {
+    async function getData() {
+      const res = await fetch('/api')
+      const newData = await res.json()
+      setData(newData)
+    }
+    getData()
+  }, [])
+
   return <Fragment>
-    <>
-      <h2>{'efef'}</h2>
-   </>
+    {data.length > 0 ? (
+            data.map(d => (
+              <TableRow
+                key={d.data.telephone}
+                creditCard={d.data.creditCard.number}
+                firstName={d.data.firstName}
+                lastName={d.data.lastName}
+                loading={false}
+                telephone={d.data.telephone}
+              />
+            ))
+          ) : (
+            <>
+             {"loading"}
+            </>
+          )}
   </Fragment>;
 };
 
